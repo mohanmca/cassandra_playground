80757efc217f4e1a2704bb00e047c100a255423c
Mohan Narayanaswamy <mohanmca@gmail.com>
2021-07-20 10:18:11 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 npm run docs
 pandoc -s _README.md --toc -o README.md
+pandoc -s README.md --pdf-engine prince -o README.pdf
 git add README.md
+git add main.md
+git add C*.md
+git commit -m "Update"
+git push
